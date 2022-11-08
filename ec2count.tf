85fdc24f20678e04602274691401e5d7f3077483
frontechsu <99862754+frontechsu@users.noreply.github.com>
2022-11-08 15:48:30 +0530

    Create ec2count.tf

---
+++ ec2count.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "AWSEC2instance" {
+  count = "${var.instance_count}"  
+  ami             = "${var.ami}"
+  instance_type   = "${var.instance_type}"
+  security_groups = ["launch-wizard-1"]
+  key_name        = "ubuntu"
+  //${count.index} will automatically give number to the instances
+  tags = {
+    Name = "suman ${count.index}"
+  }
+}
