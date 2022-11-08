742a6a36861b65eb5e6ce9893e6c81d02952ca62
frontechsu <99862754+frontechsu@users.noreply.github.com>
2022-11-08 15:36:45 +0530

    Create ec2var.tf

---
+++ ec2var.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "AWSEC2instance" {
+  count = "${var.instance_count}"  
+  ami             = "${var.ami}"
+  instance_type   = "${var.instance_type}"
+  security_groups = ["launch-wizard-1"]
+  key_name        = "ubuntu"
+  tags = {
+    Name = "suman var file"
+  }
+}
